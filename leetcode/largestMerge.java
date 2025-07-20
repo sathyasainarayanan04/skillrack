bcaaf460127ac5199a19650447b95d13cc2a004b
sathyasainarayanan04 <sathyaofficial0404@gmail.com>
2025-07-20 09:20:04 +0530

    Create largestMerge.java

---
+++ leetcode/largestMerge.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String largestMerge(String word1, String word2) {
+        if(word1.length()==0 && word2.length()==0) return "";
+        if(word1.length()==0) return word2;
+        if(word2.length()==0) return word1;
+        if(word1.compareTo(word2)>0)
+        return word1.charAt(0) + largestMerge(word1.substring(1),word2);
+        else 
+        return word2.charAt(0) + largestMerge(word1, word2.substring(1));
+    }
+}
