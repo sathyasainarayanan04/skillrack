e16bf8b0f18203145d6aa32bcccdf0b47bc0d93f
sathyasainarayanan04 <sathyaofficial0404@gmail.com>
2025-08-05 20:33:09 +0530

    Create rearrangeArray.java

---
+++ leetcode/rearrangeArray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int[] result = new int[nums.length];
+        int positiveIndex = 0;
+        int negativeIndex = 1;
+        for(int num : nums){
+            if(num>0){
+                result[positiveIndex] = num;
+                positiveIndex+=2;
+            }
+            else{
+                result[negativeIndex] = num;
+                negativeIndex+=2;
+            }
+        }
+        return result;
+    }
+}
