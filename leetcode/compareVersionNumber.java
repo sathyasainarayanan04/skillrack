ce699bcdd8661462940f0ae9c195dcdfcaad6e40
sathyasainarayanan04 <sathyaofficial0404@gmail.com>
2025-09-24 06:55:19 +0530

    Create compareVersionNumber.java

---
+++ leetcode/compareVersionNumber.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int index1 =0, index2 = 0;
+        int length1 = version1.length(),length2 = version2.length();
+        int number1 = 0, number2 = 0;
+        while(index1 < length1 || index2 < length2){
+        while(index1 < length1 && version1.charAt(index1)!='.'){
+            number1 = number1 * 10 + (version1.charAt(index1) - '0');
+            index1++;
+        }
+        while(index2 < length2 && version2.charAt(index2)!='.'){
+            number2 = number2 * 10 + (version2.charAt(index2) - '0');
+            index2++;
+        }
+        if(number1 < number2) return -1;
+        if(number1 > number2) return 1;
+        number1 = 0; number2 =0;
+        index1++;
+        index2++; 
+        }
+        return 0;
+    }
+}
